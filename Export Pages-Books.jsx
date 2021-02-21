387c75ab89bc7b5c2c224903daabd38efda6a561
Nate Lovell <Natelovell99@gmail.com>
2021-02-21 15:32:38 -0700

    Create Export Pages-Books.jsx

---
+++ Export Pages-Books.jsx
@@ -0,0 +1,10 @@
+﻿var document = app.activeDocument;
+// ExportFormat.PNG_FORMAT
+// ExportFormat.EPUB
+// ExportFormat.HTML
+// ExportFormat.JPG
+// ExportFormat.PDF_TYPE
+
+document.exportFile(ExportFormat.PNG_FORMAT, File("~/Desktop/documentExport.png"), false);
+
+// book.exportFile();
